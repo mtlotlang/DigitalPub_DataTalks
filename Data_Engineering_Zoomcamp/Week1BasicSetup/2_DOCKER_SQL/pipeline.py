dc8dca1381bd101ebd23a34e10a3ae972ced5eaa
mtlotlang <mtlotlang@consultant.com>
2022-04-07 09:59:27 +0200

    first commit

---
+++ Data_Engineering_Zoomcamp/Week1BasicSetup/2_DOCKER_SQL/pipeline.py
@@ -0,0 +1,13 @@
+import sys
+
+import pandas as pd
+
+print(sys.argv)
+
+day = sys.argv[1]
+
+
+#import pandas as pd
+# some fance stuff with pandas
+
+print(f'job finished successfully for day = f{day}')
