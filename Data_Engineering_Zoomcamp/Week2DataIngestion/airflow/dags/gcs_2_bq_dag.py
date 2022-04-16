1fcc6e7a750f9b95f7c28e58f1a9d97fccfa1527
mtlotlang <mtlotlang@consultant.com>
2022-04-16 22:02:56 +0200

    update

---
+++ Data_Engineering_Zoomcamp/Week2DataIngestion/airflow/dags/gcs_2_bq_dag.py
@@ -0,0 +1,71 @@
+import os
+import logging
+
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator, BigQueryInsertJobOperator
+from airflow.providers.google.cloud.transfers.gcs_to_gcs import GCSToGCSOperator
+
+PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
+BUCKET = os.environ.get("GCP_GCS_BUCKET")
+
+path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'trips_data_all')
+
+default_args = {
+    "owner": "airflow",
+    "start_date": days_ago(1),
+    "depends_on_past": False,
+    "retries": 1,
+}
+
+# NOTE: DAG declaration - using a Context Manager (an implicit way)
+with DAG(
+    dag_id="live_coded_gcs_2_bg_dag",
+    schedule_interval="@daily",
+    default_args=default_args,
+    catchup=False,
+    max_active_runs=1,
+    tags=['dtc-de'],
+) as dag:
+
+    #for colour in ["yellow","green"]:{colour}
+    gcs_2_gcs_task = GCSToGCSOperator(
+        task_id="gcs_2_gcs_task",
+        source_bucket=BUCKET,
+        source_object="raw/yellow_tripdata*.parquet",
+        destination_bucket=BUCKET,
+        destination_object="yellow/", 
+        move_object=True
+    )
+    
+    gcs_2_bq_ext_task = BigQueryCreateExternalTableOperator(
+        task_id="gcs_2_bq_ext_task",
+        table_resource={
+            "tableReference": {
+                "projectId": PROJECT_ID,
+                "datasetId": BIGQUERY_DATASET,
+                "tableId": f"external_yellow_tripdata",
+            },
+            "externalDataConfiguration": {
+                "autodetect" : True,
+                "sourceFormat": "PARQUET",
+                "sourceUris": [f"gs://{BUCKET}/yellow/*"],
+            },
+        },
+    )
+    CREATE_PART_TBL_QUERY=f"CREATE OR REPLACE TABLE {BIGQUERY_DATASET}.yellow_tripdata_partitoned \
+        PARTITION BY DATE(tpep_pickup_datetime) AS \
+        SELECT * FROM {BIGQUERY_DATASET}.external_yellow_tripdata;"
+    
+    bq_ext_2_part_task = BigQueryInsertJobOperator(
+        task_id="bq_ext_2_part_task",
+        configuration={
+             "query": {
+                 "query": CREATE_PART_TBL_QUERY,
+                 "useLegacySql": False,
+        }
+    },
+)
+    #download_dataset_task >> format_to_parquet_task >> local_to_gcs_task >> bigquery_external_table_task
+    gcs_2_gcs_task >> gcs_2_bq_ext_task >> bq_ext_2_part_task
